526e08ac86a82cb864e7990e33130b1f342a80ff
renatosouzav <renato.souza@intmed.com.br>
2023-05-25 12:16:48 -0300

    add logic to store data from github into database through the RepositorySerializer

--- repositories/serializers.py
+++ repositories/serializers.py
@@ -1,9 +1,37 @@
 from rest_framework import serializers
+from django.utils.functional import cached_property
 
 from .models import Commit, Repository
+from .utils import GitHubData
+from requests import exceptions
 
 
 class RepositorySerializer(serializers.ModelSerializer):
+
+    @cached_property
+    def _data_from_github(self):
+        repo_name = self.initial_data['name']
+        user = self.context['request'].user
+        social = user.social_auth.get(provider='github')
+        token = social.extra_data['access_token']
+        return GitHubData(token, user, repo_name).get_data_github()
+
+    def create(self, validated_data):
+        repository_instance = Repository.objects.create(**validated_data)
+        github_data_for_model = []
+        for commit in self._data_from_github:
+            commit.update({'repository': repository_instance})
+            github_data_for_model.append(Commit(**commit))
+
+        Commit.objects.bulk_create(github_data_for_model)
+        return repository_instance
+
+    def validate_name(self, name):
+        try:
+            self._data_from_github
+            return name
+        except exceptions.HTTPError as error:
+            raise serializers.ValidationError('Repository does not exist!')
     class Meta:
         model = Repository
         fields = ('name',)
