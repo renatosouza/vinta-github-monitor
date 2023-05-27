5e08bb26bccf876bc0ff95923ac306ac89663be3
renatosouzav <renato.souza@intmed.com.br>
2023-05-27 16:19:59 -0300

    create and add CustomPagination that brings page_size information

---
+++ repositories/pagination.py
@@ -0,0 +1,15 @@
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
+
+class CustomPagination(PageNumberPagination):
+    page_size = 10
+
+    def get_paginated_response(self, data):
+        return Response({
+            'page_size': self.page_size,
+            'count': self.page.paginator.count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
--- repositories/views.py
+++ repositories/views.py
@@ -5,13 +5,14 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Commit, Repository
 from .serializers import CommitSerializer, RepositorySerializer
 from .filters import CommitFilter
+from .pagination import CustomPagination
 
 
 class CommitListView(generics.ListAPIView):
     serializer_class = CommitSerializer
     queryset = Commit.objects.all()
     permission_classes = [IsAuthenticated]
-    pagination_class = None
+    pagination_class = CustomPagination
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = CommitFilter
 
