69f7e45380c527fb665a20f5ec5d1a69f545a788
renatosouzav <renato.souza@intmed.com.br>
2023-05-31 13:49:10 -0300

    make repository name unique

--- repositories/models.py
+++ repositories/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.db.utils import IntegrityError
 
 
 class Repository(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
--- repositories/serializers.py
+++ repositories/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 from .models import Commit, Repository
 
 
 class RepositorySerializer(serializers.ModelSerializer):
+    name = serializers.CharField(
+        validators=[UniqueValidator(
+            queryset=Repository.objects.all(),
+            message='A repository with this name already exists!'
+        )]
+    )
 
     class Meta:
         model = Repository
